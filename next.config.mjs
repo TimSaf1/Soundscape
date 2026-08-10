8cc79321b777b7df156a449e056abba2dfaf4aa1
TimSaf1 <saftim08@yandex.ru>
2026-08-10 15:47:57 +0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,27 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,
-  },
+  // Генерирует готовую папку /out для хостинга статики 
+  output: 'export',
+  
+  // Отключаем image.domains, если не используем загрузку картинок из интернета через next/image
   images: {
     unoptimized: true,
   },
-}
+  
+  // Настройка прокси заголовков для Railway
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "X-Content-Type-Options",
+            value: "nosniff"
+          }
+        ],
+      },
+    ];
+  },
+};
 
-export default nextConfig
+export default nextConfig;
