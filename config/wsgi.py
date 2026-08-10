23deac75e1b1c0c80a537667f3d4defc8a8f3913
TimSaf1 <saftim08@yandex.ru>
2026-08-10 15:28:49 +0300

    Delete config directory

--- config/wsgi.py
+++
@@ -1,8 +0,0 @@
-import os
-
-from django.core.wsgi import get_wsgi_application
-
-# ВАЖНО: Название должно совпадать с вашим пакетом настроек (папкой config)
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
-
-application = get_wsgi_application()
