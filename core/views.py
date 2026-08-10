7c82fc3f44e3e6fff01accd76ef397faa9515be9
TimSaf1 <saftim08@yandex.ru>
2026-08-10 15:28:59 +0300

    Delete core directory

--- core/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-def index(request):
-    # Возвращает простую HTML-строку или рендерит шаблон templates/core/index.html
-    return HttpResponse("<h1>Привет! Сайт работает на Railway!</h1>")
-
-# def about(request):
-#     return render(request, 'core/about.html')
